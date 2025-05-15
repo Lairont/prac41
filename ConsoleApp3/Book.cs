f3c420cda3353c440ad87f60fe0b099314cf31c9
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-15 11:21:07 +0300

    Add files via upload

---
+++ ConsoleApp3/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Book
+    {
+        private string title, author, publisher;
+        public Book(string title, string author, string publisher)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Publisher = publisher;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+
+        public override string ToString()
+        {
+            return $"Название{Title}\nАвтор{Author}\nИздательсво{Publisher}";
+        }
+    }
+}
